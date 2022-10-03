4b98db5a2f251630d58c72caf12522decee7be3c
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-10-03 20:03:41 -0300

    Requisito 5

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import Login from '../pages/Login';
+import App from '../App';
 
 describe('Testando comportamentos da página de Login', () => {
-  it('Elementos do componente Login estão present: ', () => {
-    const { history } = renderWithRouterAndRedux(<Login />);
+  it('Elementos do componente Login estão present: ', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
 
     const titleLogin = screen.getByRole('heading', { name: /wallet/i });
     const fieldEmail = screen.getByRole('textbox');
@@ -13,14 +14,30 @@ describe('Testando comportamentos da página de Login', () => {
     const btnLogin = screen.getByRole('button', { name: /entrar/i });
     const { pathname } = history.location;
 
+    const regexEmail1 = /\S+@\S+\.\S+/;
+    const verifiedEmail1 = regexEmail1.test(fieldEmail.value);
+    expect(verifiedEmail1).toBe(false);
+
+    userEvent.type(fieldEmail, 'teste123@qualquercoisa.com');
+    userEvent.type(fieldPassword, '123456');
+
+    const regexEmail2 = /\S+@\S+\.\S+/;
+    const verifiedEmail2 = regexEmail2.test(fieldEmail.value);
+
     expect(titleLogin).toBeInTheDocument();
+
     expect(fieldEmail).toBeInTheDocument();
+    expect(verifiedEmail2).toBe(true);
+
     expect(fieldPassword).toBeInTheDocument();
+    expect(fieldPassword.value.length).toBe(6); // Gostaria de deixar dinâmico, o importânte é ser > 6
+
     expect(btnLogin).toBeInTheDocument();
     expect(pathname).toBe('/');
 
-  // verificar se é um email válido;
-  // verificar se a senha tem mais de 6 dígitos;
+    userEvent.click(btnLogin);
+    await waitFor(() => { expect(history.location.pathname).toBe('/carteira'); });
+
   // verificar se o botão ativa quando essas duas condições forem atendidas;
   // verificar se quando o botão esta ativo e é clicado as funções dispatch, updateEmail e push são chamadas;
   // verificar se quando o botão esta ativo e é clicado muda para a rota /carteira;
