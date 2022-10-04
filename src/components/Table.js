1337f5008a5f7f83ac1558da182582fe8188268a
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-10-04 10:26:54 -0300

    Requisito 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Table.css';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    // console.log(expenses);
     return (
       <table className="tabela">
         <thead>
@@ -18,9 +22,49 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          {expenses.map((e) => (
+            <tr key={ e.id }>
+              <td>{e.description}</td>
+              <td>{e.tag}</td>
+              <td>{e.method}</td>
+              <td>{Number(e.value).toFixed(2)}</td>
+              <td>{e.exchangeRates[e.currency].name}</td>
+              <td>{Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+              <td>
+                {(Number(e.value) * Number(e.exchangeRates[e.currency].ask)).toFixed(2)}
+              </td>
+              <td>{e.exchangeRates[e.currency].codein}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btnEdit"
+                >
+                  Editar
+                </button>
+                <button
+                  type="button"
+                  className="btnDelet"
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+Table.defaultProps = {
+  expenses: [],
+};
+
+const mapStateToProps = ({ wallet }) => wallet;
+
+export default connect(mapStateToProps)(Table);
