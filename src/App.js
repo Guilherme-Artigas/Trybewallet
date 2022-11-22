051cf66732753242207a76ee8916fa2c01e9d636
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-11-21 23:29:27 -0300

    mudando a rota de / para /trybewallet

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wallet from './pages/Wallet';
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/Trybewallet" component={ Login } />
       <Route path="/carteira" component={ Wallet } />
     </Switch>
   );
