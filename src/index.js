b34a51f035bf017b6f7d4200a657d55fd6c27ca2
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-05-01 09:18:07 -0300

    style: mudança nas fontes, e centralização do conteúdo

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
@@ -16,8 +14,3 @@ ReactDOM
       </Provider>
     </BrowserRouter>,
   );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
